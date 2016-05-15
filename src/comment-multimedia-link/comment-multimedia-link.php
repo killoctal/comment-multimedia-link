997046c144c2edc9448a5301fa24d2a68fdd0333
Gabriel Schlozer <g.schlozer@gmail.com>
2016-05-15 21:06:56 +0200

    created plugin header

--- src/comment-multimedia-link/comment-multimedia-link.php
+++ src/comment-multimedia-link/comment-multimedia-link.php
@@ -1,2 +1,8 @@
 <?php
-
+/*
+Plugin Name: Comment Multimedia Link
+Plugin URI: https://github.com/killoctal/comment-multimedia-link
+Description: Plugin for WordPress for add a multimedia link in comment field
+Version: 0.1.1
+Author: Gabriel Schlozer
+*/
